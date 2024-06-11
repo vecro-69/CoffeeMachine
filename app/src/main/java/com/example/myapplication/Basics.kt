11fd5029d5a5f48f48a5bec3185f2806d581ccca
Vecro <victor.taulean@outlook.com>
2024-06-11 14:16:01 +0300

    Adding data class for coffee details and update the functions.

--- app/src/main/java/com/example/myapplication/Basics.kt
+++ app/src/main/java/com/example/myapplication/Basics.kt
@@ -1,30 +1,48 @@
 package com.example.myapplication
 
+import java.time.temporal.TemporalAmount
+
+data class CoffeeDetails (
+    val sugarCount: UInt,
+    val userName: String,
+    val cupSize: String,
+    val creamAmount: UInt
+)
+
 fun main() {
     //val dogDaisy = Dog("Daisy")
     //dogDaisy.age = 2u
     //dogDaisy.bark()
 
+    // Call Function
+    val coffeeDetails = askForCoffeeDetails()
+    makeCoffee(coffeeDetails)
+}
+
+fun askForCoffeeDetails(): CoffeeDetails {
     println("What is your name?: ")
     val userName = readln()
     println("How many pieces of sugar do you want?: ")
     val sugarCount = readln().toUInt()
+    println("What shall be the size of the coffee?: ")
+    val cupSize = readln()
+    println("How much cream do you want?: ")
+    val creamAmount = readln().toUInt()
 
-    // Call Function
-    makeCoffee(userName, sugarCount)
+    return CoffeeDetails(sugarCount, userName, cupSize, creamAmount)
 }
 
 // Define Function
-fun makeCoffee(userName : String, sugarCount : UInt) {
-    when (sugarCount) {
+fun makeCoffee(coffeeDetails: CoffeeDetails) {
+    when (coffeeDetails.sugarCount) {
         1u -> {
-            println("Coffee with $sugarCount spoon of sugar for $userName.")
+            println("Coffee with ${coffeeDetails.sugarCount} spoon of sugar for ${coffeeDetails.userName}. Cup size is ${coffeeDetails.cupSize} and it contains ${coffeeDetails.creamAmount} pieces of cream.")
         }
         0u -> {
-            println("Coffee with no sugar for $userName.")
+            println("Coffee with no sugar for ${coffeeDetails.userName}. Cup size is ${coffeeDetails.cupSize} and it contains ${coffeeDetails.creamAmount} pieces of cream.")
         }
         else -> {
-            println("Coffee with $sugarCount spoons of sugar for $userName.")
+            println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.userName}. Cup size is ${coffeeDetails.cupSize} and it contains ${coffeeDetails.creamAmount} pieces of cream.")
         }
     }
 
